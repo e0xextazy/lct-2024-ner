1f62b397f1d0b69d545944bcee14a305725112db
Klim Gordeev <gklimg@gmail.com>
2024-06-16 19:00:37 +0800

    add dataset text to locust test

--- service/tests/locustfile.py
+++ service/tests/locustfile.py
@@ -1,10 +1,17 @@
 from locust import HttpUser, task
+import pandas as pd
+from tqdm import tqdm
+
+texts = pd.read_csv('../../data/gt_test.csv')['processed_text']
 
 
 class HelloWorldUser(HttpUser):
     @task
     def test_predict(self):
-        self.client.post("/predict", json={'text': 'Test text'})
+        for text in tqdm(texts):
+            response = self.client.post("/predict", json={'text': text})
+            response.raise_for_status()
+        print('OK')
 
     def on_start(self):
         response = self.client.post("/predict", json={'text': 'Text 1'})
